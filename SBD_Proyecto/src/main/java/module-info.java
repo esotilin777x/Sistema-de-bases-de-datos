f8efbb7ba03dcfb131aea3dbdf6bbf5a18d74713
Andrés Bohórquez <andbvill@espol.edu.ec>
2024-08-13 01:05:31 -0500

    fxml basico

--- SBD_Proyecto/src/main/java/module-info.java
+++ SBD_Proyecto/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module com.mycompany.sbd_proyecto {
     requires javafx.controls;
+    requires java.base;
+    requires javafx.fxml; 
+    opens com.mycompany.sbd_proyecto to javafx.fxml;
     exports com.mycompany.sbd_proyecto;
 }
